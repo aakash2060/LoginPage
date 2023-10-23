4ff5aacfe6895411af190c60b4257d5a333635f1
Aakash Poudel <aakash.poudel.900@gmail.com>
2023-10-23 02:20:53 -0500

    3rd Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <div className ="Login">
     <div className = 'LoginBox'>
       <div className = "appLogo">
         <img src= "./images/logotrans.png" alt="Logo"/>
         </div>
-    <div className = 'LoginHeader'>Login </div>
+    <div className = 'LoginHeader'>    User Login </div>
       <div className = "inputs">
         <input className = "email" placeholder = "Email"/>
         <input 
         className = "password" 
         placeholder = "Password"
-        type = "password"
-        />
+        type = {showPassword ? 'text' : 'password'}/>
+        <a onClick ={() => setShowPassword(!showPassword)}
+                className= "showPasswordLink">
+          {showPassword ? 'Hide Password' : 'Show Password'}
+          </a>
+          <a href="/forgot-password" className="forgotPasswordLink">
+            Forgot Password?
+          </a>
       </div>
       <div style ={{display: "flex", justifyContent: "center", color: 'black'}}>
         <button className = "submitbutton">Login</button>
