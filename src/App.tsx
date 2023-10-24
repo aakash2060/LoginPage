fa7d27bfd214caee7c5870d670e408e30f5fd5c5
Aakash Poudel <aakash.poudel.900@gmail.com>
2023-10-24 02:11:57 -0500

    New Change

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,17 @@
-import React, { useState } from 'react';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Login from "./components/Login/Login";
+import SignUp from "./components/Signup/SignUp";
+import React from "react";
 
 function App() {
-  const [showPassword, setShowPassword] = useState(false);
   return (
-    <div className ="Login">
-    <div className = 'LoginBox'>
-      <div className = "appLogo">
-        <img src= "./images/logotrans.png" alt="Logo"/>
-        </div>
-    <div className = 'LoginHeader'>    User Login </div>
-      <div className = "inputs">
-        <input className = "email" placeholder = "Email"/>
-        <input 
-        className = "password" 
-        placeholder = "Password"
-        type = {showPassword ? 'text' : 'password'}/>
-        <a onClick ={() => setShowPassword(!showPassword)}
-                className= "showPasswordLink">
-          {showPassword ? 'Hide Password' : 'Show Password'}
-          </a>
-          <a href="/forgot-password" className="forgotPasswordLink">
-            Forgot Password?
-          </a>
-      </div>
-      <div style ={{display: "flex", justifyContent: "center", color: 'black'}}>
-        <button className = "submitbutton">Login</button>
-        <button className = "signupbutton">Sign Up</button>
-      
-    </div>
-    </div>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Signup/SignUp.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+export const SignUp: React.FC = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log("Form Data:", formData);
+  };
+  return (
+    <div className="Signup">
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            value={formData.firstName}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            value={formData.lastName}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
+export {};
